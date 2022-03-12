e8329699984b3cd68bb93970591c5e037372ad50
V. A. Canales L <victorcanlima@gmail.com>
2022-03-12 09:37:34 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Categorization challenge for Baker Huges Data Science Hackathon Mexico 2022',
+    author='Canales Lima V. A., Piña K., Ramos López O., Santos Estudillo P.',
+    license='MIT',
+)
